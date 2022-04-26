1812539019f82e56122547495b9af1664b7a5c76
Mehmet Nail Kocagoz <mehmetnailkocagoz@gmail.com>
2022-04-26 23:27:39 +0200

    26/04

--- src/interviewQuestions8/Q05_WrapperClassTask.java
+++ src/interviewQuestions8/Q05_WrapperClassTask.java
@@ -10,4 +10,21 @@ public class Q05_WrapperClassTask {
     // ve bu int'nin iki katını döndürür
     // bu yöntemi wrapper sınıfıyla overload et : Integer
 
+    public static void main(String[] args) {
+        twice(5);
+        System.out.println("primitive data type :" +twice(5)); //primitive data type :10
+
+        Integer sayi= 10;
+        System.out.println("nonprimitive data type : " +twice(sayi)); //nonprimitive data type : 20
+
+    }
+
+    private static int twice(int sayi) {
+        return sayi * 2;
+    }
+
+    private static Integer twice(Integer sayi) {
+        return sayi * 2;
+    }
+
 }
